b50d87e3e0c811f08e8a4f1f54863a4362c27e1f
Marcus <ratrateroo@gmail.com>
2021-03-25 19:56:51 +0800

    started backend

---
+++ backend-graphql-booking/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+//app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res, next) => {
+	res.send('Hello World!');
+});
+
+app.listen(3000);
