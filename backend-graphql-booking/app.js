ea0e884caac896df870a579d6b365c7bf25f3273
Mark <ratrateroo@gmail.com>
2021-04-11 23:44:23 +0800

    added new file for the resolvers

--- backend-graphql-booking/app.js
+++ backend-graphql-booking/app.js
@@ -20,74 +20,7 @@ app.use(
 	'/graphql',
 	graphqlHTTP({
 		schema: ,
-		rootValue: {
-			events: () => {
-				return Event.find()
-					.then((events) => {
-						return events.map((event) => {
-							return { ...event._doc, _id: event.id };
-						});
-					})
-					.catch((err) => {
-						throw err;
-					});
-			},
-			createEvent: (args) => {
-				const event = new Event({
-					title: args.eventInput.title,
-					description: args.eventInput.description,
-					price: +args.eventInput.price,
-					date: new Date(args.eventInput.date),
-					creator: 'idgoeshere',
-				});
-				let createdEvent;
-				return event
-					.save()
-					.then((result) => {
-						createdEvent = {
-							...result._doc,
-							_id: result._doc._id.toString(),
-						};
-						return User.findById('idgoeshere');
-					})
-					.then((user) => {
-						if (!user) {
-							throw new Error('User not found.');
-						}
-						user.createdEvents.push(event);
-						return user.save();
-					})
-					.then((result) => {
-						return createdEvent;
-					})
-					.catch((err) => {
-						console.log(err);
-						throw err;
-					});
-			},
-			createUser: (args) => {
-				return User.findOne({ email: args.userInput.email })
-					.then((user) => {
-						if (user) {
-							throw new Error('User exists already.');
-						}
-						return bcrypt.hash(args.userInput.password, 12);
-					})
-					.then((hashedPassword) => {
-						const user = new User({
-							email: args.userInput.email,
-							password: hashedPassword,
-						});
-						return user.save();
-					})
-					.then((result) => {
-						return { ...result._doc, password: null, _id: result.id };
-					})
-					.catch((err) => {
-						throw err;
-					});
-			},
-		},
+		rootValue: ,
 		graphiql: true,
 	})
 );
