852978f05d5e3d565a3c1e4552d207a28d6bf32f
Mark <ratrateroo@gmail.com>
2021-04-11 23:21:55 +0800

    added booking related mutations

--- backend-graphql-booking/app.js
+++ backend-graphql-booking/app.js
@@ -63,6 +63,8 @@ app.use(
         type RootMutation {
             createEvent(eventInput: EventInput): Event
 			createUser(userInput: UserInput): User
+			bookEvent(eventId: ID!): Booking!
+    cancelBooking(bookingId: ID!): Event!
         }
 
         schema {
